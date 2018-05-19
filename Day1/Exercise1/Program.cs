cbd6152fa04b5da278dc9c721cfc9d8fe3a0627e
Stephanie Rankin <ssrankin@umich.edu>
2018-05-19 16:41:57 -0400

    commit for Exercise 1

---
+++ Day1/Exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int status = 2;
+        
+            if (status == 1)
+            {
+            Console.WriteLine("Alive");
+            }
+            else if (status == 2)
+            {
+            Console.WriteLine("Zombie");
+            }
+            else if (status == 3)
+            {
+            Console.WriteLine("Dead");
+            }
+            else if (status == 4)
+            {
+            Console.WriteLine("Unknown");
+            }
+            Console.WriteLine("Exact value of a is: {0}", status);
+            Console.ReadLine();
+        }
+    }
+}
