3917564a8c596ca7e179f6b1f5d824c534205507
Stephanie Rankin <ssrankin@umich.edu>
2018-05-22 23:27:28 -0400

    Day 1 Assignments

---
+++ Day1/Exercise1b/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int caseSwitch = 3;
+
+            switch (caseSwitch)
+            {
+                case 1: 
+                    Console.WriteLine("Alive");
+                    break;
+                case 2:
+                    Console.WriteLine("Zombie");
+                    break;
+                case 3:
+                    Console.WriteLine("Dead");
+                    break;
+                default:
+                    Console.WriteLine("Unknown");
+                    break;
+            }
+        }
+    }
+}
---
+++ Day1/Exercise1c/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise1c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int status = 2;
+            /*while loop execution*/
+
+            while 
+        }
+    }
+}
