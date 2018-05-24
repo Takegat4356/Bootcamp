48c684fc4c3240b41c0fc46b0887dee22f0f439a
Stephanie Rankin <ssrankin@umich.edu>
2018-05-23 22:20:29 -0400

    Exercise1c commit

--- Day1/Exercise1c/Program.cs
+++ Day1/Exercise1c/Program.cs
@@ -6,10 +6,39 @@ namespace Exercise1c
     {
         static void Main(string[] args)
         {
-            int status = 2;
-            /*while loop execution*/
-
-            while 
-        }
+            int index = 0;
+            //Continue looping until internal condition is met
+            //while loop
+            while (true)
+            {
+                int value = ++index;
+                //Check to see if the limit is hit.
+               
+                int status = 2;
+        
+                if (status == 1)
+                {
+                Console.WriteLine("Alive");
+                break;
+                }
+                else if (status == 2)
+                {
+                Console.WriteLine("Zombie");
+                break;
+                }
+                else if (status == 3)
+                {
+                Console.WriteLine("Dead");
+                break;
+                }
+                else if (status == 4)
+                {
+                Console.WriteLine("Unknown");
+                break;
+                }  
+            Console.WriteLine("Exact value of a is: {0}", status);
+            Console.ReadLine();                 
+            }                    
+        }       
     }
 }
