02ce739c1e5afdc4f87016807d847edafa5b006f
Stephanie Rankin <ssrankin@umich.edu>
2018-05-26 14:43:00 -0400

    Day 2 commit

--- Day2/Zombie.Simulator/Program.cs
+++ Day2/Zombie.Simulator/Program.cs
@@ -6,10 +6,18 @@ namespace Zombie.Simulator
     {        
         static void Main(string[] args)
         {
-            Person Mary = new Person(); // new instance of the Person class
-            Person person1 = new Person();
+            //Class was on Saturday 5/26/2018
+            Person Mary = new Person(); // 1st in class Exercise new instance of the Person class
+            Person person1 = new Person();// 2nd in class Exercise new instance of the Person class
             person1.DistanceTraveled = 5;
             Console.WriteLine(person1.DistanceTraveled);
+            person1.Walk(10);
+            Console.WriteLine (person1.DistanceTraveled);
+
+            Person person2 = new Person();
+            person2.Walk(10);
+            Console.WriteLine (person2.DistanceTraveled);
+            Console.WriteLine (person1.DistanceTraveled);
         }        
     }
     public class Person //this can also be in another file
@@ -17,7 +25,21 @@ namespace Zombie.Simulator
                 public int DistanceTraveled  {get; set;}
                 public Person()
                 {
-                    Console.Write ("A new person has been created.");
+                    Console.Write ("A new person has been created. ");
+                }
+                public void Walk(int minutes)
+                {
+                    //use void, when we are not running anything
+                    int distance = CalculateDistance(minutes);
+                    DistanceTraveled = DistanceTraveled + minutes * 2;
+                    //just a random calculation
+                    //local variable in this method
+                }
+
+                private int CalculateDistance(int minutes)
+                {
+                    //the details you want to hide, make private
+                    return minutes *2;
                 }
         }  
 
