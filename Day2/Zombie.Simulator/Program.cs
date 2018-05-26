bda4f391bc4830f0ee4a280ffdbe2595b0974d2c
Stephanie Rankin <ssrankin@umich.edu>
2018-05-26 14:08:42 -0400

    Commit Day2

--- Day2/Zombie.Simulator/Program.cs
+++ Day2/Zombie.Simulator/Program.cs
@@ -6,11 +6,15 @@ namespace Zombie.Simulator
     {        
         static void Main(string[] args)
         {
-            Person Mary = new Person("Mary"); // new instance of the Person class
+            Person Mary = new Person(); // new instance of the Person class
+            Person person1 = new Person();
+            person1.DistanceTraveled = 5;
+            Console.WriteLine(person1.DistanceTraveled);
         }        
     }
     public class Person //this can also be in another file
         {
+                public int DistanceTraveled  {get; set;}
                 public Person()
                 {
                     Console.Write ("A new person has been created.");
