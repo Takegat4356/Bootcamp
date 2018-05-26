f547364dbf54be60bd59ddf07243e3e8b18674fd
Stephanie Rankin <ssrankin@umich.edu>
2018-05-26 12:52:09 -0400

    Day 2 commit

---
+++ Day2/Zombie.Simulator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zombie.Simulator
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Person Mary = new Person("Mary"); // new instance of the Person class
+        }        
+    }
+    public class Person //this can also be in another file
+        {
+                public Person()
+                {
+                    Console.Write ("A new person has been created.");
+                }
+        }  
+
+}
