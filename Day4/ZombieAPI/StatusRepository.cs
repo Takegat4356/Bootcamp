becb87a4e76a524d8ba5411d8376bedf8a2766e4
Stephanie Rankin <ssrankin@umich.edu>
2018-06-09 16:48:06 -0400

    Commit Day4

---
+++ Day4/ZombieAPI/StatusRepository.cs
@@ -0,0 +1,23 @@
+using System.Data;
+using System.Collections.Generic;
+using Dapper;
+using System.Linq;
+
+namespace ZombieAPI
+{
+public class StatusRepository: Repository
+    {   
+        //create a method
+        public List<Status> GetAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+
+                string sql = "SELECT p.FirstName, p.LastName, ps.StatusDescription FROM persondatabase.person p INNER JOIN persondatabase.personstatus ps ON p.PersonStatusID = ps.PersonStatusID"; //works
+
+                return dbConnection.Query<Status>(sql,commandType:CommandType.Text).ToList();
+            }
+        }
+    }
+}
