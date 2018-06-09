4c5d1527df182ffbd5e92a50841f4c54f6aecfa1
Stephanie Rankin <ssrankin@umich.edu>
2018-06-09 15:10:04 -0400

    Commit Day 4

--- Day4/Zombie.Simulator4/StatusRepository.cs
+++ Day4/Zombie.Simulator4/StatusRepository.cs
@@ -14,8 +14,6 @@ public class StatusRepository: Repository
             {
                 dbConnection.Open();
 
-                /*string sql = "SELECT p.FirstName, p.LastName, ps.StatusDescription" + "FROM persondatabase.person p" + "INNER JOIN persondatabase.personstatus ps"+ "ON p.PersonStatusID = ps.PersonStatusID";*/
-
                 string sql = "SELECT p.FirstName, p.LastName, ps.StatusDescription FROM persondatabase.person p INNER JOIN persondatabase.personstatus ps ON p.PersonStatusID = ps.PersonStatusID"; //works
 
                 return dbConnection.Query<Status>(sql,commandType:CommandType.Text).ToList();
