5ba05b6b7ebe5e6ebdc3494f3da7f260c124afd9
Stephanie Rankin <ssrankin@umich.edu>
2018-06-09 16:47:19 -0400

    Commit Day4

---
+++ Day4/ZombieAPI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ZombieAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+
+           //added from ZombieData Exercise 
+           StatusRepository repository = new StatusRepository();
+           List<Status> statuses = repository.GetAll(); 
+
+            foreach (Status status in statuses)
+            {
+                Console.WriteLine (status.FirstName + " " + 
+                status.LastName + " has a status of " + 
+                status.StatusDescription);
+            }
+            //added from ZombieData Exercise 
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ Day4/ZombieAPI/Status.cs
@@ -0,0 +1,10 @@
+
+namespace ZombieAPI
+{
+    public class Status
+    {
+        public string FirstName{get;set;}
+        public string LastName{get;set;}
+        public string StatusDescription{get;set;}
+    }
+}
