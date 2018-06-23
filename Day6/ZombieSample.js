591db79d9a5a14c23f00d811ad7ac92be245d7f5
Stephanie Rankin <ssrankin@umich.edu>
2018-06-23 13:50:47 -0400

    Day6 commit

--- Day6/ZombieSample.js
+++ Day6/ZombieSample.js
@@ -1,24 +1,36 @@
-let statuses = [4, 3, 1, 2, 4, 1]
-let names = ['John','Gary','Lisa','Sarah','Bob', 'LaJuana']
+let statuses = [
+    {status:4,name:'John'},
+    {status:2,name:'Gary'},
+    {status:1,name:'Lisa'},
+    {status:3,name:'Sarah'},
+    {status:2,name:'Bob'},
+    {status:1,name:'LaJuana'}
+];
 
-let counter = 0;
-for (let counter = 0; counter < statuses.length; counter++){
-    let status = "";
-    if (statuses [counter] === 1){
-        status = "Alive";        
-} else if (statuses [counter] === 2) {
-        status = "Zombie";
-} else if (statuses [counter] === 3) {
-        status = "Dead";
-} else {
-        status = "Unknown";
-}
-    console.log(names [counter] + " has a status of " + status);    
+for(let statuses of statuses){
+    writeString(getPersonStatusDescription(personStatus));
 }
 
+function getPersonStatusDescription(personStatus){
 
+    let status = 'Status for' + personStatus.name + 'is';    
 
+    let counter = 0;
+    for (let counter = 0; counter < statuses.length; counter++){
+        let status = "";
+        if (statuses [counter] === 1){
+                status = "Alive";        
+        } else if (statuses [counter] === 2) {
+                status = "Zombie";
+        } else if (statuses [counter] === 3) {
+                status = "Dead";
+        } else {
+                status = "Unknown";
+        }
+        return status;
+    }
+}
 
-
-
-
+function writeString(value){
+    console.log(value);
+}
