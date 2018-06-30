7c5ac9f3f548aef65bb45f2cbf70de1b3a8295b9
Stephanie Rankin <ssrankin@umich.edu>
2018-06-30 16:41:33 -0400

    commit Day 7

--- Day7/ZombieReportAng/src/app/entities/PersonStatus.ts
+++ Day7/ZombieReportAng/src/app/entities/PersonStatus.ts
@@ -1,7 +1,12 @@
 export class PersonStatus{
-    public firstName:string;    
-    public lastName:string;     
-    public statusId:number;    
-    public statusDescription:string;    
+    public FirstName:string;    
+    public LastName:string;     
+    public PersonStatusID:number;    
+    public StatusDescription:string;
+
+    //public firstName:string;    
+    //public lastName:string;     
+    //public statusId:number;    
+    //public statusDescription:string;    
     //public personId:number;  
 }
