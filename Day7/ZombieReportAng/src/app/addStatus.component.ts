5c931ffd815a9cb0d4831ed0f1d529b4f679950f
Stephanie Rankin <ssrankin@umich.edu>
2018-06-30 14:35:15 -0400

    Day 7 commit

---
+++ Day7/ZombieReportAng/src/app/addStatus.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'addStatus',
+  templateUrl: './addStatus.component.html',
+  styleUrls: ['./addStatus.component.css']
+})
+export class addStatusComponent {
+}
+
+
