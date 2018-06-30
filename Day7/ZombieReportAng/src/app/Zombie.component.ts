0c8a687bae7264ca4e1d0a4f088273cfcc786046
Stephanie Rankin <ssrankin@umich.edu>
2018-06-30 16:36:44 -0400

    commit Day 7

--- Day7/ZombieReportAng/src/app/Zombie.component.ts
+++ Day7/ZombieReportAng/src/app/Zombie.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { PersonStatus } from 'e2e/src/entities/PersonStatus';
+import { PersonStatus } from './entities/PersonStatus';
+import { PersonStatusService } from './services/PersonStatusService';
 
 @Component({
   selector: 'Zombie',
@@ -10,14 +11,20 @@ export class ZombieComponent {
 public title: string= 'Zombie Report';
 public statuses: PersonStatus[] = [];
   
-public constructor(){
-this.statuses =[
+public constructor(personStatusService: PersonStatusService) {
+  this.statuses = [];
+  personStatusService.getAll().subscribe(result => {
+    this.statuses.push(...result);
+  }, error => {
+    console.error(error)
+  });
+
+/*  this.statuses =[
   {firstName: 'Tom', lastName: 'Masters', statusId:4,statusDescription:'Unknown'},
   {firstName: 'Bob', lastName: 'Masters', statusId:2,statusDescription:'Zombie'},
   {firstName: 'John', lastName: 'Masters', statusId:1,statusDescription:'Alive'},
   {firstName: 'Joe', lastName: 'Masters', statusId:3,statusDescription:'Dead'},
   {firstName: 'Steve', lastName: 'Masters', statusId:2,statusDescription:'Zombie'}
-  ];  
+  ];  */
 }
-
 }
--- Day7/ZombieReportAng/src/app/app.module.ts
+++ Day7/ZombieReportAng/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ZombieComponent } from './Zombie.component';
 import { addStatusComponent } from './addStatus.component';
 import { HttpClient } from 'selenium-webdriver/http';
+
+import { PersonStatusService } from './services/PersonStatusService';
 
 const appRoutes:Routes = [
 {path: 'addStatus', component:addStatusComponent},
@@ -19,11 +23,13 @@ const appRoutes:Routes = [
   ],
   imports: [
     BrowserModule,
-    //HttpClientModule,
-    //FormsModule,  
+    HttpClientModule,
+    FormsModule,  
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+  PersonStatusService  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
