fb7e66d1d716391bb1d06fdfa705cf5f6b295dd9
Stephanie Rankin <ssrankin@umich.edu>
2018-06-30 13:35:40 -0400

    Commit Day 7

---
+++ Day7/ZombieReportAng/e2e/src/entities/PersonStatus.ts
@@ -0,0 +1,13 @@
+export class PersonStatus{
+
+    public firstName:string;
+    
+    public lastName:string; 
+    
+    public personStatusID:number; 
+    
+    public statusDescription:string; 
+    
+    public personId:number;  
+
+}
--- Day7/ZombieReportAng/src/app/Zombie.component.ts
+++ Day7/ZombieReportAng/src/app/Zombie.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PersonStatus } from 'e2e/src/entities/PersonStatus';
 
 @Component({
   selector: 'Zombie',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./Zombie.component.css']
 })
 export class ZombieComponent {
+public title: string= 'Zombie Report';
+public statuses: PersonStatus[] = [];
   
+public constructor(personStatusService: PersonStatusService){
+this.statuses =[
+  {firstName: 'Tom', lastName: 'Masters', statusID:4,statusDescription:'Unknown'},
+  {firstName: 'Bob', lastName: 'Masters', statusID:2,statusDescription:'Zombie'},
+  {firstName: 'John', lastName: 'Masters', statusID:1,statusDescription:'Alive'},
+  {firstName: 'Joe', lastName: 'Masters', statusID:3,statusDescription:'Dead'},
+  {firstName: 'Steve', lastName: 'Masters', statusID:2,statusDescription:'Zombie'}
+  ];  
+}
+
+
 }
