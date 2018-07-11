a2447bfe0196140ae9a2fe8b7853edf4f1608f8c
Stephanie Rankin <ssrankin@umich.edu>
2018-07-10 23:20:04 -0400

    commit Day8

--- Day8/ZombieReport/src/app/ZombieReport.component.ts
+++ Day8/ZombieReport/src/app/ZombieReport.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { PersonStatus } from './entities/PersonStatus'
+import { PersonStatusService } from './services/PersonStatusService'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'zombieReport',
+  templateUrl: './zombieReport.component.html',
+  styleUrls: ['./zombieReport.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class ZombieReportComponent {
+  public title:string = 'Zombie Status Report';
+  public statuses:PersonStatus[] = [];
+
+  public constructor(personStatusService: PersonStatusService) {
+    personStatusService.getAll().subscribe(result => {
+      for (let status of result) {
+        this.statuses.push(status);
+      }
+    }, error => { 
+      console.log(error)
+    });
+    /*
+    this.statuses = [
+      {firstName:'Tom',lastName:'Masters',statusId:4,statusDescription:'Unknown'},
+      {firstName:'Bob',lastName:'Masters',statusId:2,statusDescription:'Zombie'},
+      {firstName:'John',lastName:'Masters',statusId:1,statusDescription:'Alive'},
+      {firstName:'Joe',lastName:'Masters',statusId:3,statusDescription:'Dead'},
+      {firstName:'Steve',lastName:'Masters',statusId:2,statusDescription:'Zombie'}     
+    ];
+    */
+  }
 }
