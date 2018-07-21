c6687d1d7c720177bfda90a7a326b4ea3060b0dd
Stephanie Rankin <ssrankin@umich.edu>
2018-07-21 11:15:18 -0400

    commit Day 8

--- Day8/ZombieReport.WebAPI/Program.cs
+++ Day8/ZombieReport.WebAPI/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ZombieAPI
+namespace ZombieReport.WebAPI
 {
     public class Program
     {
